d2746b6e0e1ca447a095280626e11f03a222885d
kaitlynnchan <kaitlynnchan@outlook.com>
2021-01-09 12:12:58 -0800

    Working on ui

---
+++ app/src/main/java/com/projects/chefsjournal/ui/MainActivity.java
@@ -0,0 +1,32 @@
+package com.projects.chefsjournal.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.projects.chefsjournal.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/projects/chefsjournal/ui/RecipeActivity.java
+++ app/src/main/java/com/projects/chefsjournal/ui/RecipeActivity.java
@@ -1,14 +1,16 @@
-package com.projects.chefsjournal;
+package com.projects.chefsjournal.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.projects.chefsjournal.R;
+
+public class RecipeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recipe);
     }
 }
