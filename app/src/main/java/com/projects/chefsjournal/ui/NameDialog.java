1059c2d58cbe73504f34e5bd8bbf891c1fb549ba
kaitlynnchan <kaitlynnchan@outlook.com>
2021-01-17 19:11:53 -0800

    Allow adding folder.

---
+++ app/src/main/java/com/projects/chefsjournal/ui/NameDialog.java
@@ -0,0 +1,36 @@
+package com.projects.chefsjournal.ui;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import com.projects.chefsjournal.R;
+
+public class NameDialog extends AppCompatDialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_name, null);
+
+        Button okBtn = view.findViewById(R.id.btn_ok);
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(view)
+                .create();
+    }
+}
